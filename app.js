a6c45530d683080003715d232ff10ef4d743c38b
FlorinaRebeicaGrigoras <rebeicagrigoras25@gmail.com>
2024-05-23 15:51:19 +0100

    Getting real-time data for the clock

---
+++ app.js
@@ -0,0 +1,12 @@
+const secondsHand = document.getElementById("seconds-hand");
+const minutesHand = document.getElementById("minutes-hand");
+const hoursHand = document.getElementById("hours-hand");
+
+function getTime() {
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const minutes = now.getMinutes();
+  const hours = now.getHours();
+}
+
+getTime();
