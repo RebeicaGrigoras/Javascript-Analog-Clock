3027505379dca2f0cfa3949b99d4a74c4db4b793
FlorinaRebeicaGrigoras <rebeicagrigoras25@gmail.com>
2024-05-24 08:16:50 +0100

    Implemented final changes for real-time clock display

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ function getTime() {
   const seconds = now.getSeconds();
   const minutes = now.getMinutes();
   const hours = now.getHours();
+  const timeInterval = 6;
+
+  secondsHand.style.transform = "rotate(" + seconds * timeInterval + "deg)";
+  minutesHand.style.transform =
+    "rotate(" + (minutes * timeInterval + seconds / 10) + "deg)";
+  hoursHand.style.transform = "rotate(" + (hours * 30 + minutes / 2) + "deg)";
 }
 
-getTime();
+setInterval(getTime, 100);
